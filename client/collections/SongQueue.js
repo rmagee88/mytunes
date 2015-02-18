bda45f30788427d6225f26c13c674d5c6499ffb0
rmagee88 <rmagee88@gmail.com>
2015-02-18 15:49:07 -0800

    SongModel, SongQueue passed.

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -2,6 +2,21 @@
 var SongQueue = Songs.extend({
 
   initialize: function(){
+    // this.models.on("add", console.log("check"));
+    this.on("add", function() { if (this.models.length === 1) this.playFirst();})
+    this.on("ended", function(){
+      this.shift();
+      if(this.models.length){
+        this.playFirst();
+      }
+    });
+    this.on("dequeue", function(song){
+      this.remove(song);
+    });
+  },
+
+  playFirst: function(){
+      this.models[0].play();
   }
 
 });
